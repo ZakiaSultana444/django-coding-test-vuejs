731f42d7dfa3c844bf588afeb556f2e9286dd21e
ZakiaSultana444 <zakiaasultanaa4@gmail.com>
2024-05-26 01:03:15 +0600

    Second commit

--- django-coding-test-vuejs/django-coding-test/src/config/settings.py
+++ django-coding-test-vuejs/django-coding-test/src/config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'authentication',
-    'product'
+    'product',
+     
 ]
 
 MIDDLEWARE = [
@@ -120,8 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = '/media/'
